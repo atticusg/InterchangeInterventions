ec8115175368bbb39874196b2cb91186f62ccf8b
atticusg <atticusg@stanford.edu>
2022-06-14 12:40:28 -0700

    Update LIM_bert.py

--- LIM_bert.py
+++ LIM_bert.py
@@ -216,9 +216,10 @@ class LIMBERTClassifier(LayeredIntervenableModel):
 
 
         #unstack sources
-        sources_mask = [sources_mask[:,j,:].squeeze(1).type(torch.FloatTensor)
+        print(sources_mask.device)
+        sources_mask = [sources_mask[:,j,:].squeeze(1).type(torch.FloatTensor).to(sources_mask.device)
            for j in range(sources_mask.shape[1])]
-        sources_input = [sources_input[:,j,:].squeeze(1).type(torch.FloatTensor)
+        sources_input = [sources_input[:,j,:].squeeze(1).type(torch.FloatTensor).to(sources_mask.device)
            for j in range(sources_input.shape[1])]
         #translate intervention_ids to coordinates
         gets =  intervention_ids_to_coords[int(intervention_ids.flatten()[0])]
