00ce45dccc75956cdf1e29b5dfe708757d0bb632
atticusg <atticusg@stanford.edu>
2022-11-09 23:44:40 -0800

    Update ii_benchmark.py

--- ii_benchmark.py
+++ ii_benchmark.py
@@ -282,8 +282,8 @@ class IIBenchmarkMoNli(IIBenchmark):
             bert,
             self.model_parameters['max_length'],
             debug=self.model_parameters['debug'],
-            self.model_parameters['target_dims'],
-            self.model_parameters['target_layer'],
+            target_dims = self.model_parameters['target_dims'],
+            target_layer=self.model_parameters['target_layer'],
         )
 
 
