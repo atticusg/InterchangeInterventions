681931cdc08fcc802f3a3419b98dde6317ab427d
atticusg <atticusg@stanford.edu>
2022-11-15 19:31:29 -0800

    Update dataset_nli.py

--- dataset_nli.py
+++ dataset_nli.py
@@ -126,7 +126,7 @@ class IIT_MoNLIDataset:
         word_neutral = pmonli_neutral + nmonli_entail
 
         def get_intervention(base,source):
-            return min([i if not torch.equal(base[i], source[i]) else base.shape[0] + 42 for i in range(base.shape[0])])
+            return min([i if int(base[i]) != (source[i]) else base.shape[0] + 42 for i in range(base.shape[0])])
 
         while True:
             example = random.choice(pmonli_entail)
